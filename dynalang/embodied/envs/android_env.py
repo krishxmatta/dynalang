c0e26d57c96eda9208f7928fd2bf8b3c80a462c2
Krish Matta <self@krishxmatta.dev>
2024-03-21 17:19:10 -0400

    Discretize action space

--- dynalang/embodied/envs/android_env.py
+++ dynalang/embodied/envs/android_env.py
@@ -1,5 +1,6 @@
 import embodied
 import numpy as np
+import gym
 
 def get_explicit_spaces():
     obs_space = {
@@ -13,9 +14,8 @@ def get_explicit_spaces():
     }
 
     act_space = {
-        "action_type": embodied.Space(dtype=np.float32, shape=(2,), low=0, high=1),
-        "touch_position": embodied.Space(dtype=np.float32, shape=(2,), low=-1.0, high=1.0),
-        "reset": embodied.Space(dtype=bool, shape=(), low=False, high=True),
+        "action": embodied.Space(dtype=np.float32, shape=(299,), low=0, high=1),
+        "reset": embodied.Space(dtype=bool, shape=(), low=False, high=True)
     }
 
     return obs_space, act_space
@@ -31,21 +31,22 @@ class AndroidEnv(embodied.Env):
                  task_filename='mdp_0000.textproto'):
         from android_env import loader
         from android_env.components import config_classes
+        from android_env.wrappers.discrete_action_wrapper import DiscreteActionWrapper
         from android_env.wrappers.gym_wrapper import GymInterfaceWrapper
         from . import from_gym
 
         task_path = f"android_env/tasks/{task}/{task_filename}"
 
-        self._env = GymInterfaceWrapper(loader.load(
-                                    avd_name='avd',
-                                    android_avd_home='/Users/krishxmatta/.android/avd',
-                                    android_sdk_root='/Users/krishxmatta/Library/Android/sdk',
-                                    emulator_path='/Users/krishxmatta/Library/Android/sdk/emulator/emulator',
-                                    adb_path='/Users/krishxmatta/Library/Android/sdk/platform-tools/adb',
-                                    task_path='/Users/krishxmatta/Projects/androidenv/tasks/vokram/mdp_0000.textproto'))
+        self._env = GymInterfaceWrapper(DiscreteActionWrapper(loader.load(
+                                            avd_name='avd',
+                                            android_avd_home='/Users/krishxmatta/.android/avd',
+                                            android_sdk_root='/Users/krishxmatta/Library/Android/sdk',
+                                            emulator_path='/Users/krishxmatta/Library/Android/sdk/emulator/emulator',
+                                            adb_path='/Users/krishxmatta/Library/Android/sdk/platform-tools/adb',
+                                            task_path='/Users/krishxmatta/Projects/androidenv/tasks/vokram/mdp_0000.textproto')))
 
         self.observation_space = self._env.observation_space
-        self.action_space = self._env.action_space
+        self.action_space = gym.spaces.Dict({"action": gym.spaces.Box(0, 299, (), np.int32)})
 
         self.wrappers = [from_gym.FromGym]
 
@@ -55,6 +56,10 @@ class AndroidEnv(embodied.Env):
         return obs
 
     def step(self, action):
+        action = action.copy()
+        action["action_id"] = action["action"]
+        del action["action"]
+
         obs, reward, done, info = self._env.step(action)
         obs["is_read_step"] = False
         return obs, reward, done, info
